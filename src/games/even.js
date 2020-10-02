475a575fdc25f5f688b0b66729646bde8565e963
Anton Minin <aminin@progredis.ru>
2020-10-02 15:09:33 +0300

    Убрал дублирование кода

--- src/games/even.js
+++ src/games/even.js
@@ -2,19 +2,15 @@ import { random } from '../random.js';
 
 const isEven = (n) => n % 2 === 0;
 
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-const even = (options = {}) => {
-  const { minNumber = 0, maxNumber = 100 } = options;
-  const generateQuestion = () => {
-    const number = random(minNumber, maxNumber);
-    return [number, ['no', 'yes'][+isEven(number)]];
-  };
-  return {
-    description,
-    generateQuestion,
-    ...options,
-  };
+export const withPredicate = (predicate, { minNumber = 0, maxNumber = 100 }) => () => {
+  const number = random(minNumber, maxNumber);
+  return [number, ['no', 'yes'][+predicate(number)]];
 };
+
+const even = (options = {}) => ({
+  description: 'Answer "yes" if the number is even, otherwise answer "no".',
+  generateQuestion: withPredicate(isEven, options),
+  ...options,
+});
 
 export default even;
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,23 +1,14 @@
-import { random } from '../random.js';
+import { withPredicate } from './even';
 
 const isPrime = (a, b = 2) => {
   if (b ** 2 > a) return true;
   return a % b ? isPrime(a, b + 1) : false;
 };
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-const prime = (options = {}) => {
-  const { minNumber = 0, maxNumber = 100 } = options;
-  const generateQuestion = () => {
-    const number = random(minNumber, maxNumber);
-    return [number, ['no', 'yes'][+isPrime(number)]];
-  };
-  return {
-    description,
-    generateQuestion,
-    ...options,
-  };
-};
+const prime = (options = {}) => ({
+  description: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+  generateQuestion: withPredicate(isPrime, options),
+  ...options,
+});
 
 export default prime;
